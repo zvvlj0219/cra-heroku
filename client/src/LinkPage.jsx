2ec83449a003b8d042ad841b7037ee429dbb2431
zvvlj0219 <s00302448@gmail.com>
2022-04-22 01:46:13 +0900

    setting for production

--- client/src/LinkPage.jsx
+++ client/src/LinkPage.jsx
@@ -1,7 +1,10 @@
+import typescript from './assets/TypeScript.png'
+
 const LinkPage = () => {
   return (
     <div>
       <h2 style={{textAlign:'center'}}>this is link page</h2>
+      <img src={typescript} alt='' />
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const axios = require('axios')
+const path = require('path')
 
 const app = express()
 require('dotenv').config()
@@ -16,8 +16,26 @@ app.get('/app', async (req, res) => {
   return res.status(200).send({ username: 'zvvlj0219'})
 })
 
+
+// handle production
+if (process.env.NODE_ENV === 'production') {
+  // static folder
+  app.use(express.static(path.resolve(__dirname, './build')))
+  // handle spa
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build/index.html'))
+  })
+}
+
 const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log(process.env.NODE_ENV)
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+// comment
+// SET NODE_ENV=production
+// set ではなく SET
+
+// commit後、yarn run ejectを実行し、ビルドの設定ファイルを生成。
+// path.jsのパスをserver直下に変更する
