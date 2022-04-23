2fcbf8aba2cb01cfa5454f749d6ce2041019da03
zvvlj0219 <s00302448@gmail.com>
2022-04-23 23:47:43 +0900

    build2

--- client/src/FirstView.jsx
+++ client/src/FirstView.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import logo from './logo.svg';
 import './App.css';
 
 const url = process.env.NODE_ENV === 'production'
@@ -27,18 +26,6 @@ const FirstView = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <p>
           {
             text ?
